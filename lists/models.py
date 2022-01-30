a1b4cd1d51778455e1dec63a7e5de7bc071178bd
kimhan1113 <kimhan1113@naver.com>
2022-01-30 23:58:44 +0900

    list models commit

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from core.models import TimeStampedModel
+from users.models import User
+
+
+class List(TimeStampedModel):
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
