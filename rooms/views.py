b4ab060f6df1389421764d95ba9f8d31971a1ed6
kimhan1113 <kimhan1113@naver.com>
2022-02-14 22:16:27 +0900

    next Previous page navigation commit

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
-from datetime import datetime
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 # Create your views here.
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    print(dir(request.GET))
+    # page = int(request.GET.get("page", 1))
+
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+
     # print(request.GET.clear())
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", {"potato": all_rooms})
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "potato": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count),
+        },
+    )
