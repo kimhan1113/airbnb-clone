bd4172b6354d8b45b5513a2ffcdea37f03f36003
kimhan1113 <kimhan1113@naver.com>
2022-04-06 23:32:05 +0900

    header part two

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,8 @@ PROJECT_APPS = [
 THIRD_PARTY_APPS = [
     "django_countries",
     "django_seed",
+
+    # 'webpack_boilerplate',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
@@ -88,6 +90,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': ['django_tailwind_app/templates'],
         "DIRS": [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -152,6 +155,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "frontend/build"),
+# ]
+#
+# WEBPACK_LOADER = {
+#     'MANIFEST_FILE': os.path.join(BASE_DIR, "frontend/build/manifest.json"),
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # path('', TemplateView.as_view(template_name="index.html")),
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
     path("users/", include("users.urls", namespace="users")),
