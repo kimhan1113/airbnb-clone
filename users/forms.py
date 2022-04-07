e50cc00cb30bee78d51e2a6522e78b788b675c2f
kimhan1113 <kimhan1113@naver.com>
2022-04-08 01:22:44 +0900

    signup

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from . import models
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -20,26 +22,78 @@ class LoginForm(forms.Form):
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
 
-# class SignUpForm(forms.ModelForm):
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+
+        }
+
+        # password = forms.CharField(
+        #     widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+        # )
+        # password1 = forms.CharField(
+        #     widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+        # )
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}), label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
+
+# class SignUpForm(UserCreationForm):
+#     username = forms.EmailField(label="Email")
+#
 #     class Meta:
 #         model = models.User
 #         fields = ("first_name", "last_name", "email")
+#         widgets = {
+#             "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+#             "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+#             "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+#         }
 #
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+#         password = forms.CharField(
+#             widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+#         )
+#         password1 = forms.CharField(
+#             widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+#         )
 #
 #     def clean_password1(self):
 #         password = self.cleaned_data.get("password")
 #         password1 = self.cleaned_data.get("password1")
-#
-#     def clean_email(self):
-#         email = self.cleaned_data.get("email")
-#         try:
-#             models.User.objects.get(email=email)
-#             raise forms.ValidationError("User already exists with that email")
-#         except models.User.DoesNotExist:
-#             return email
-#
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
 #
 #     def save(self, *args, **kwargs):
 #         user = super().save(commit=False)
@@ -48,8 +102,4 @@ class LoginForm(forms.Form):
 #         user.username = email
 #         user.set_password(password)
 #         user.save()
-
-class SignUpForm(UserCreationForm):
-    username = forms.EmailField(label="Email")
-
 
